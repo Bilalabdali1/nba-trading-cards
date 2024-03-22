eb20a268f5999a3c9cf85e35986a1218540d8918
Bilal <bilalabdali1234@gmail.com>
2024-03-22 16:56:50 -0400

    Change urls stripe

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,8 +18,8 @@ const Cart = ({ cart, updateCart, removeItem, totals }) => {
     const { error } = await stripe.redirectToCheckout({
       lineItems,
       mode: 'payment',
-      successUrl: 'http://localhost:3000/', // URL to redirect to after successful payment
-      cancelUrl: 'http://localhost:3000/', // URL to redirect to if payment is canceled
+      successUrl: 'https://nba-trading-cards.vercel.app/', // URL to redirect to after successful payment
+      cancelUrl: 'https://nba-trading-cards.vercel.app/', // URL to redirect to if payment is canceled
     });
 
     // Handle any errors
